8b9a6e9ff59c37c1a4738eb042c8ec8c9d5bfdb3
Aymane-Arfaoui <124536672+Aymane-Arfaoui@users.noreply.github.com>
2023-05-30 12:46:30 -0400

    Add files via upload
    Simple Fibonacci Sequence

---
+++ Fibonnaci.java
@@ -0,0 +1,24 @@
+package week1Exercices;
+
+//Simple fibonnaci sequence
+
+
+public class Fibonnaci {
+    
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+    
+    public static void main(String[] args) {
+        int n = 10; 
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+}
+
